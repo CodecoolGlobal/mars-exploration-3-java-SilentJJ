6b3fa4344c85adee19a57f9dd7dd626d3d775727
SilentJJ <testy@freemail.hu>
2023-11-08 12:58:39 +0100

    add question model

--- src/main/java/com/codecool/stackoverflowtw/dao/model/Question.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/model/Question.java
@@ -1,4 +1,40 @@
 package com.codecool.stackoverflowtw.dao.model;
 
+import java.time.LocalDate;
+
 public class Question {
+
+    private final String title;
+    private final String body;
+    private int like;
+    private LocalDate createdAt;
+    private int numOfAnswers;
+
+    public Question(String title, String body, int like, LocalDate createdAt, int numOfAnswers) {
+        this.title = title;
+        this.body = body;
+        this.like = like;
+        this.createdAt = createdAt;
+        this.numOfAnswers = numOfAnswers;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void addLike() {
+        like++;
+    }
 }
